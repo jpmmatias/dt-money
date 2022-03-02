8b8d5ecc6bca6b9fb680ed49f1f0e1a5f473c341
João Pedro Matias <joao.matias@rebase.com.br>
2022-03-02 15:18:00 -0300

    Adicionando table transactions e mirage js

--- src/components/TransactionsTable/TransactionsTable.tsx
+++ src/components/TransactionsTable/TransactionsTable.tsx
@@ -1,7 +1,47 @@
+import { useEffect } from 'react';
 import { Container } from './styles';
 
 const TransactionsTable = () => {
-	return <Container>TransactionsTable</Container>;
+	useEffect(() => {
+		fetch('http://localhost:3000/api/transactions')
+			.then((res) => res.json())
+			.then((data) => console.log(data));
+	}, []);
+
+	return (
+		<Container>
+			<table>
+				<thead>
+					<tr>
+						<th>Título</th>
+						<th>Valor</th>
+						<th>Categoria</th>
+						<th>Data</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>Desenvolvimento de website</td>
+						<td>R$ 10.000</td>
+						<td>Desenvolvimento</td>
+						<td>20/02/2021</td>
+					</tr>
+					<tr>
+						<td>Desenvolvimento de website</td>
+						<td>R$ 10.000</td>
+						<td>Desenvolvimento</td>
+						<td>20/02/2021</td>
+					</tr>
+					<tr>
+						<td>Desenvolvimento de website</td>
+						<td>R$ 10.000</td>
+						<td>Desenvolvimento</td>
+						<td>20/02/2021</td>
+					</tr>
+				</tbody>
+			</table>
+		</Container>
+	);
 };
 
 export default TransactionsTable;
